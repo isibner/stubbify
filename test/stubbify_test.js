e6d121536d9e5d722fb95eda4f3036fb4572dca7
jusjmkim <justinjmkim@gmail.com>
2014-12-28 22:28:15 -0500

    test default and other regexes in stubbify

--- test/stubbify_test.js
+++ test/stubbify_test.js
@@ -9,13 +9,10 @@ var stubbify = require('../stubbify.js');
 var assert = chai.assert;
 
 describe('#stubbify', function () {
-  var fixturesPath = './test/fixtures/';
-  var testFile = fixturesPath + 'example.js';
-  var stubbifiedFile = fixturesPath + 'tmp/test/fixtures/example.js';
-  var wantedFile = fixturesPath + 'stubbified.js';
-  var destinationDir = fixturesPath + 'tmp';
-  var beginningStub = Parser.DEFAULT_START_REGEX;
-  var endingStub = Parser.DEFAULT_END_REGEX;
+  var testFile = './test/fixtures/example.js';
+  var stubbifiedFile = './test/fixtures/tmp/test/fixtures/example.js';
+  var wantedFile = './test/fixtures/stubbified.js';
+  var destinationDir = './test/fixtures/tmp';
 
   var readTestFile = function (fileToRead) {
     var data = fs.readFileSync(fileToRead);
@@ -26,6 +23,9 @@ describe('#stubbify', function () {
   var testFileLines, stubbifiedFileLines, wantedFileLines;
 
   before('read test, stubbified, and wanted files', function (done) {
+    var beginningStub = Parser.DEFAULT_START_REGEX;
+    var endingStub = Parser.DEFAULT_END_REGEX;
+
     stubbify(testFile, destinationDir, beginningStub, endingStub, function (err) {
       assert.isNull(err);
       testFileLines = readTestFile(testFile);
@@ -36,7 +36,7 @@ describe('#stubbify', function () {
   });
 
   after(function () {
-    del.sync(destinationDir);
+    del.sync(destinationDir + '*');
   });
 
   it('is different from original file', function () {
@@ -44,6 +44,34 @@ describe('#stubbify', function () {
   });
 
   it('stubs correctly', function () {
-    assert.deepEqual(stubbifiedFileLines, wantedFileLines);
+    assert.deepEqual(wantedFileLines, stubbifiedFileLines);
+  });
+
+  it('uses default regexes', function (done) {
+    var defaultRegExpTestDir = './test/fixtures/tmp2';
+    var defaultRegExpTestFile = './test/fixtures/tmp2/test/fixtures/example.js';
+    stubbify(testFile, defaultRegExpTestDir, undefined, undefined, function (err) {
+      assert.isNull(err);
+      var defaultRegExpTestLines = readTestFile(defaultRegExpTestFile);
+      assert.deepEqual(defaultRegExpTestLines, wantedFileLines);
+      done();
+    });
+  });
+
+  it('can take other regexes', function (done) {
+    var beginStub = new RegExp('^.*\<\!\-*[\\s]*STUB[\\s]*\-*\>', 'i');
+    var endStub = new RegExp('^.*\<\!\-*[\\s]*ENDSTUB[\\s]*\-*\>', 'i');
+    var htmlTestFile = './test/fixtures/example.html';
+    var wantedHtmlFile = './test/fixtures/stubbified.html';
+    var stubbifiedHtmlTestFile = './test/fixtures/tmp/test/fixtures/example.html';
+    stubbify(htmlTestFile, destinationDir, beginStub, endStub, function (err) {
+      assert.isNull(err);
+      var htmlTestLines = readTestFile(htmlTestFile);
+      var stubbifiedHtmlTestLines = readTestFile(stubbifiedHtmlTestFile);
+      var wantedHtmlLines = readTestFile(wantedHtmlFile);
+      assert.notDeepEqual(htmlTestLines, stubbifiedHtmlTestLines);
+      assert.deepEqual(wantedHtmlLines, stubbifiedHtmlTestLines);
+      done();
+    });
   });
 });
