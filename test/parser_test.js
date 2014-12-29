3eb98878bb3faf75e0a2f2d08e7908050e879584
jusjmkim <justinjmkim@gmail.com>
2014-12-29 12:07:15 -0500

    take out redundant notStrictEqual

--- test/parser_test.js
+++ test/parser_test.js
@@ -36,65 +36,42 @@ describe('#parser', function () {
   });
 
   it('does not take in beginStub', function (done) {
-    var testString = '// STUB';
-    readable.push(testString);
+    readable.push('//STUB');
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString, writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
   });
 
   it('is case insensitive', function (done) {
-    var testString = '//stub';
-    readable.push(testString);
+    readable.push('//stub');
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString, writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
   });
 
   it('does not take in text after beginStub', function (done) {
-    var testString = '//STUB';
-    var testString2 = 'hello world';
-    var testString3 = '//ENDSTUB';
     readable.push('//STUB');
     readable.push('hello world');
     readable.push('//ENDSTUB');
     readable.end();
 
-    var unparsedString = [testString, testString2, testString3].join('\n');
     writable.on('finish', function () {
-      assert.notStrictEqual(unparsedString, writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
   });
 
   it('does not take in text before beginStub in same line', function (done) {
-    var testString = 'hello world//STUB';
-    readable.push(testString);
+    readable.push('hello world //STUB');
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString + '\n', writable.result);
-      assert.strictEqual('', writable.result);
-      done();
-    });
-  });
-
-  it('stubs out text directly before beginStub', function (done) {
-    var testString = 'hello world //STUB';
-    readable.push(testString);
-    readable.end();
-
-    writable.on('finish', function () {
-      assert.notStrictEqual(testString + '\n', writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
