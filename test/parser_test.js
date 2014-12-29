67be0899fabcae896b6d35c9e4c6bbc56b4b1197
jusjmkim <justinjmkim@gmail.com>
2014-12-29 02:55:42 -0500

    Make style changes to tests such as joining test strings and deleting unnecessary tests

--- test/parser_test.js
+++ test/parser_test.js
@@ -41,7 +41,7 @@ describe('#parser', function () {
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString + '\n', writable.result);
+      assert.notStrictEqual(testString, writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
@@ -53,7 +53,7 @@ describe('#parser', function () {
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString + '\n', writable.result);
+      assert.notStrictEqual(testString, writable.result);
       assert.strictEqual('', writable.result);
       done();
     });
@@ -63,14 +63,12 @@ describe('#parser', function () {
     var testString = '//STUB';
     var testString2 = 'hello world';
     var testString3 = '//ENDSTUB';
-    readable.push(testString);
-    readable.push(testString2);
-    readable.push(testString3);
+    readable.push('//STUB');
+    readable.push('hello world');
+    readable.push('//ENDSTUB');
     readable.end();
 
-    var unparsedString = testString + '\n' +
-                         testString2 + '\n' +
-                         testString3 + '\n';
+    var unparsedString = [testString, testString2, testString3].join('\n');
     writable.on('finish', function () {
       assert.notStrictEqual(unparsedString, writable.result);
       assert.strictEqual('', writable.result);
@@ -103,54 +101,36 @@ describe('#parser', function () {
   });
 
   it('leaves in text above beginStub', function (done) {
-    var testString = 'hello world';
-    var testString2 = '//STUB';
-    readable.push(testString);
-    readable.push(testString2);
+    readable.push('hello world');
+    readable.push('//STUB');
     readable.end();
 
     writable.on('finish', function () {
-      assert.notStrictEqual(testString + '\n' + testString2 + '\n', writable.result);
       assert.strictEqual('hello world\n', writable.result);
       done();
     });
   });
 
   it('leaves in text below endStub', function (done) {
-    var testString = '//STUB';
-    var testString2 = '//ENDSTUB';
-    var testString3 = 'hello world';
-    readable.push(testString);
-    readable.push(testString2);
-    readable.push(testString3);
+    readable.push('//STUB');
+    readable.push('//ENDSTUB');
+    readable.push('hello world');
     readable.end();
 
-    var unparsedString = testString + '\n' +
-                         testString2 + '\n' +
-                         testString3 + '\n';
     writable.on('finish', function () {
-      assert.notStrictEqual(unparsedString, writable.result);
       assert.strictEqual('hello world\n', writable.result);
       done();
     });
   });
 
   it('can take in two beginStubs', function (done) {
-    var testString = '//STUB';
-    var testString2 = '//ENDSTUB';
-    var testString3 = 'hello world';
-    readable.push(testString);
-    readable.push(testString);
-    readable.push(testString2);
-    readable.push(testString3);
+    readable.push('//STUB');
+    readable.push('//STUB');
+    readable.push('//ENDSTUB');
+    readable.push('hello world');
     readable.end();
 
-    var unparsedString = testString + '\n' +
-                         testString + '\n' +
-                         testString2 + '\n' +
-                         testString3 + '\n';
     writable.on('finish', function () {
-      assert.notStrictEqual(unparsedString, writable.result);
       assert.strictEqual('hello world\n', writable.result);
       done();
     });
